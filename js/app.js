336ebec3bb8dd06bcc82a5b7495f23502c14b589
Olga <79081032777@mail.ru>
2018-06-09 03:51:07 +0600

    add createElement func

--- js/app.js
+++ js/app.js
@@ -17,49 +17,65 @@ function addTodoItem(evt){
   addInput.value = '';
 }
 
+// созадим функцию-помощник для создания элементов. Это паттер проектирования "фасад"
+// когда мы берем трудоемкую операцию и оборачиваем ее в красивую упаковку, которой очень удобно пользоваться
+// вся jQuery - один большой фасад для DOM
+// props - объект со свойствами элемента
+// после свойств можно передать дочерние элементы, воспользуется оператором rest
+function createElement(tagName, props, ...children) {
+  const element = document.createElement(tagName);
+
+  /*
+
+  //можно воспользоваться циклом for-in что бы перебрать все свойства объекта
+  for (let prop in props) {
+    element[prop] = props[prop]; // element.type = props['type'] или props.type; потом свойство className
+  }
+  // но проблема в том, что в методе for-in окажутся и другие свойства, которые попадут в объект при наследовании
+  // для того, что бы перебрать только свойства самого объекта нужно будет вставить проверку
+  // является ли это свойство свойством самого объекта или перешло по наследованию
+
+  */
+
+  // вместо это используем метод глобального объекта keys
+  Object.keys(props).forEach(key => element[key] = props[key]); //keys верет массив со свойствами объекта. Свойство = значение
+
+  // добавим функционал по добавлению детей, ребенок может быть либо простым текстом, либо DOM-элементом
+  //console.log(tagName, children);
+
+  if (children.length > 0) { //проверим есть ли у нас дети, тк children - массив
+    children.forEach(child => {
+      if(typeof child === 'string') { // мы не можем добвлять строку как child, тк это не объект, который является узелом - DOM-элементом
+        child = document.createTextNode(child);                     // создадим сами дом-узел из текста
+      }
+      element.appendChild(child);
+    })
+  }
+
+  return element;
+}
 
 // функция создает очередной элемент для отображения задачи
 function createTodoItem(title) {
-  const checkbox = document.createElement('input');
-  checkbox.type = 'checkbox';
-  checkbox.className = 'checkbox';
+  const checkbox = createElement('input', {type: 'checkbox', className: 'checkbox'});
+  const label = createElement('label', {className: 'title'}, title); // innerText - child
+  const editInput = createElement('input', {type: 'text', className: 'textfield'});
+  const editButton = createElement('button', {className: 'edit'}, 'Изменить');
+  const deleteButton = createElement('button', {className:'delete'}, 'Удалить');
+  const listItem = createElement('li', {className: 'todo-item'}, checkbox, label, editInput, editButton, deleteButton);
 
-  const label = document.createElement('label');
-  label.innerText = title;
-  label.className = 'title';
-
-  const editInput = document.createElement('input');
-  editInput.type = 'text';
-  editInput.className = 'textfield';
-
-  const editButton = document.createElement('button');
-  editButton.innerText = 'Изменить';
-  editButton.className = 'edit';
-
-  const deleteButton = document.createElement('button');
-  deleteButton.innerText = 'Удалить';
-  deleteButton.className = 'delete';
-
-  const listItem = document.createElement('li');
-  listItem.className = 'todo-item';
-
-  listItem.appendChild(checkbox);
-  listItem.appendChild(label);
-  listItem.appendChild(editInput);
-  listItem.appendChild(editButton);
-  listItem.appendChild(deleteButton);
+  // тк мы используем оператор rest-оператор оставшихся параметров, то сюда мы можем отправить аргументы через запятую
+  // они уже будут массивом в функции
 
   bindEvents(listItem); // подписались на события
-
-  console.log(listItem);
   return listItem;
 }
 
 
 // функция обработчик принимает объект событие
 function toggleTodoItem ({ target }) { // воспользуемся реуструктуризацией - не будем указывать evt и evt.target, а укажем объект и в нем сразу нужное нам свойство
-  console.log(target); //с помощью объекта событие / или this получим доступ к элементу
-  console.log(this);
+  // console.log(target); //с помощью объекта событие / или this получим доступ к элементу
+  // console.log(this);
 
   //но нам нужен не сам чекбокс, а элемент li этой задачи - родитель чекбокса
   const listItem = this.parentNode;
@@ -76,7 +92,6 @@ function editTodoItem() {
 
   //когда находимся в режиме редактирования нужно поменять название задачи на текущее значение из поля
   if (isEditing) {
-    console.log(this);
     title.innerText = editInput.value;
     this.innerText = 'Изменить'; //будем менять название кнопки с сохранить на изменить
                                   //this - это наша кнопка изменить, тк на нее мы и нажимали
@@ -118,3 +133,6 @@ function main() {
 }
 
 main();
+
+// app в самом простом виде. Все в глобальной среде, все попадает в глобальный объектникакой инкапсуляции нет.
+// Решить пролбему можно с помощью паттерна модули - IIFE
